b357254b43df16f548fdbfbac4fc2c9d451ea93a
unknown <petratech1830@gmail.com>
2025-11-25 22:31:07 +0700

    new

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.webtech.learningkorea"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
