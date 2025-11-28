709ba2960ccddc2b22e5a8d590aa4c71b190b78f
Admin <petratech1830@gmail.com>
2025-11-28 18:34:55 +0700

    fix xp bug

--- app/src/main/java/com/webtech/learningkorea/ui/screens/HomeViewModel.kt
+++ app/src/main/java/com/webtech/learningkorea/ui/screens/HomeViewModel.kt
@@ -19,6 +19,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
